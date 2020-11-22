d2f2fc2f645355ab4402aaf5240392fc0b8a86c0
Ali Nguyen <alin57717@gmail.com>
2020-11-22 14:22:37 -0800

    Correct error to include comment model into app

--- models/index.js
+++ models/index.js
@@ -1,6 +1,7 @@
 const User = require('./User');
-const Post = require("./Post");
+const Post = require('./Post');
 const Vote = require('./Vote');
+const Comment = require('./Comment');
 
 // create associations
 User.hasMany(Post, {
@@ -54,4 +55,5 @@ Post.hasMany(Vote, {
   Post.hasMany(Comment, {
     foreignKey: 'post_id'
   });
+
   module.exports = { User, Post, Vote, Comment };
