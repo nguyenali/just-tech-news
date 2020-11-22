edb5b84650eb5a7697a34be0896df0aac31441c4
Ali Nguyen <alin57717@gmail.com>
2020-11-22 00:15:51 -0800

    Create the comment model

--- models/index.js
+++ models/index.js
@@ -38,4 +38,20 @@ User.hasMany(Vote, {
 Post.hasMany(Vote, {
     foreignKey: 'post_id'
   });
-module.exports = { User, Post, Vote };
+
+  Comment.belongsTo(User, {
+    foreignKey: 'user_id'
+  });
+  
+  Comment.belongsTo(Post, {
+    foreignKey: 'post_id'
+  });
+  
+  User.hasMany(Comment, {
+    foreignKey: 'user_id'
+  });
+  
+  Post.hasMany(Comment, {
+    foreignKey: 'post_id'
+  });
+  module.exports = { User, Post, Vote, Comment };
