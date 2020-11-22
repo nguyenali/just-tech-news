8f4da058b4a4b200af445fba250321ad01e96188
Ali Nguyen <alin57717@gmail.com>
2020-11-21 16:44:34 -0800

    hook up to server using router

--- models/User.js
+++ models/User.js
@@ -1,3 +1,8 @@
+const { Model, DataTypes } = require('sequelize');
+const sequelize = require('../config/connection');
+
+// create our User model
+class User extends Model {}
 // define table columns and configuration
 User.init(
     
